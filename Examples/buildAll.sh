857f2012b4c2e2ac7013a313b32c68ffb03e14fb
Raynaldo Rivera <wild@inbox.com>
2010-02-16 03:29:51 +0000

    Fixed the linux build version.

--- Examples/buildAll.sh
+++ Examples/buildAll.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 cd Tutorial1_2DSound
-g++ main.cpp -o ../../bin/linux-x86/2DSound -lcAudio
+g++ main.cpp -o ../../bin/linux-x86/Tutorial1_2DSound -lcAudio
 cd ../
 cd Tutorial2_3DSound
-g++ main.cpp -o ../../bin/linux-x86/3DSound -lcAudio
+g++ main.cpp -o ../../bin/linux-x86/Tutorial2_3DSound -lcAudio
 cd ../
 cd Tutorial3_MemoryPlayback
-g++ main.cpp -o ../../bin/linux-x86/MemoryPlayback -lcAudio
+g++ main.cpp -o ../../bin/linux-x86/Tutorial3_MemoryPlayback -lcAudio
 cd ../
+cd Tutorial4_AudioCapture
+g++ main.cpp -o ../../bin/linux-x86/Tutorial4_AudioCapture -lcAudio
+cd ../
+cd Tutorial5_AudioEffects
+g++ main.cpp -o ../../bin/linux-x86/Tutorial5_AudioEffects -lcAudio
+cd ../ 
+cd Tutorial6_CustomEventHandler
+g++ main.cpp cTestHandler.cpp -o ../../bin/linux-x86/Tutorial6_CustomEventHandler -lcAudio
+cd ../ 
+cd Tutorial7_CustomLogReceiver
+g++ main.cpp cTestLogReceiver.cpp -o ../../bin/linux-x86/Tutorial7_CustomLogReceiver -lcAudio
+cd ../ 
