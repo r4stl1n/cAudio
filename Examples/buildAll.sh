ce77c9f932eef9e0afa0f9d9f3e415b87cec7bc1
Raynaldo Rivera <wild@inbox.com>
2010-02-20 08:11:18 +0000

    Added the makefiles for linux...

--- Examples/buildAll.sh
+++ Examples/buildAll.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cd Tutorial1_2DSound
-g++ main.cpp -o ../../bin/linux-x86/Tutorial1_2DSound -lcAudio
+make
 cd ../
-cd Tutorial2_3DSound
-g++ main.cpp -o ../../bin/linux-x86/Tutorial2_3DSound -lcAudio
+cd Tutorial2_3DSound;
+make
 cd ../
-cd Tutorial3_MemoryPlayback
-g++ main.cpp -o ../../bin/linux-x86/Tutorial3_MemoryPlayback -lcAudio
+cd Tutorial3_MemoryPlayback;
+make
 cd ../
-cd Tutorial4_AudioCapture
-g++ main.cpp -o ../../bin/linux-x86/Tutorial4_AudioCapture -lcAudio
+cd Tutorial4_AudioCapture;
+make
 cd ../
-cd Tutorial5_AudioEffects
-g++ main.cpp -o ../../bin/linux-x86/Tutorial5_AudioEffects -lcAudio
+cd Tutorial5_AudioEffects;
+make
 cd ../ 
-cd Tutorial6_CustomEventHandler
-g++ main.cpp cTestHandler.cpp -o ../../bin/linux-x86/Tutorial6_CustomEventHandler -lcAudio
+cd Tutorial6_CustomEventHandler;
+make
 cd ../ 
 cd Tutorial7_CustomLogReceiver
-g++ main.cpp cTestLogReceiver.cpp -o ../../bin/linux-x86/Tutorial7_CustomLogReceiver -lcAudio
+make
 cd ../ 
