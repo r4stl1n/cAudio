51e11d93b944c1268a43cab8e36bb78d0fac0a10
Joshua Jones <kilauea.jones@gmail.com>
2010-02-27 22:17:03 +0000

    Added IPhone platform define

--- include/cAudioPlatform.h
+++ include/cAudioPlatform.h
@@ -1,19 +1,23 @@
 #ifndef CAUDIOPLATFORM_H_INCLUDED
 #define CAUDIOPLATFORM_H_INCLUDED
 
-#ifndef CAUDIO_PLATFORM_WIN
+#if !defined( CAUDIO_PLATFORM_WIN )
 #	if defined( WIN32 ) || defined( _WINDOWS ) || defined(_WIN32)
 #		define CAUDIO_PLATFORM_WIN
 #	endif
 #endif
 
-#ifndef CAUDIO_PLATFORM_MAC
+#if !defined( CAUDIO_PLATFORM_MAC ) || !defined( CAUDIO_PLATFORM_IPHONE )
 #   if defined( __APPLE__ ) || defined( __APPLE_CC__ )
-#      define CAUDIO_PLATFORM_MAC
+#		if __ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__ >= 30000 || __IPHONE_OS_VERSION_MIN_REQUIRED >= 30000
+#			define CAUDIO_PLATFORM_IPHONE
+#		else
+#			define CAUDIO_PLATFORM_MAC
+#		endif
 #   endif
 #endif
 
-#ifndef CAUDIO_PLATFORM_LINUX
+#if !defined( CAUDIO_PLATFORM_LINUX )
 #	if defined(_UNIX) || defined(__linux)
 #		define CAUDIO_PLATFORM_LINUX
 #	endif
